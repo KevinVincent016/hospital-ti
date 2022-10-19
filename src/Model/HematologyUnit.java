85655daf27af4e4ce045a9f181e16cab00d1f1d7
KevinVincent016 <103274956+KevinVincent016@users.noreply.github.com>
2022-10-19 09:21:28 -0500

    add HematologyUnit to Model

---
+++ src/Model/HematologyUnit.java
@@ -0,0 +1,64 @@
+package Model;
+import Structures.HeapMax;
+
+public class HematologyUnit {
+
+    private HeapMax<Patient> HPrioQueue = new HeapMax<>();
+
+    private ReceptionUnit receptionUnit = new ReceptionUnit();
+
+    public HematologyUnit() {
+    }
+
+    public HeapMax<Patient> getHPrioQueue() {
+        return HPrioQueue;
+    }
+
+    public void setHPrioQueue(HeapMax<Patient> HPrioQueue) {
+        this.HPrioQueue = HPrioQueue;
+    }
+
+    public void addToQueue(Patient p) {
+        boolean flag = false;
+        for (int i = 0; i < receptionUnit.getDBSIZE(); i++) {
+            if (receptionUnit.getDatabase().search(i) != null && receptionUnit.getDatabase().search(i).getCedula() == p.getCedula()) {
+                HPrioQueue.insert(p.getPrioridad(), p);
+                flag = true;
+                break;
+            } else {
+                flag = false;
+            }
+        }
+        if (!flag) {
+            for (int i = 0; i < receptionUnit.getDBSIZE(); i++) {
+                if (receptionUnit.getDatabase().search(i) == null) {
+                    receptionUnit.getDatabase().insert(i, p);
+                    HPrioQueue.insert(p.getPrioridad(), p);
+                    break;
+                }
+            }
+        }
+    }
+
+    public void removePacient() {
+        if (HPrioQueue.isEmpty() == true) {
+            System.out.println("There is no patient to remove");
+            receptionUnit.addToQueue(HPrioQueue.extractMax());
+        } else {
+            receptionUnit.addToQueue(HPrioQueue.extractMax());
+        }
+    }
+
+    public void printQueue() {
+        if (getHPrioQueue().getSize() == 0) {
+            System.out.println("There is no patient on the queue");
+        } else {
+            System.out.println(
+                    "Name: " + getHPrioQueue().maximun().getNombre() + "\n"
+                            + "Age: " + getHPrioQueue().maximun().getEdad() + "\n"
+                            + "ID: " + getHPrioQueue().maximun().getCedula() + "\n"
+                            + "Level of Priority: " + getHPrioQueue().maximun().getPrioridad() + "\n"
+            );
+        }
+    }
+}
